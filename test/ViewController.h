b374af473f6f5253c7205912014699c34c685d2f
jeet11 <kumargupta@Kumars-Mac-mini-2.local>
2015-04-25 17:23:56 +0530

    initial commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Kumar Gupta on 17/02/15.
+//  Copyright (c) 2015 JsTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Kumar Gupta on 17/02/15.
+//  Copyright (c) 2015 JsTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
